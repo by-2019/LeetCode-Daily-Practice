9016486edb8ea12a19cc2cbdcee4467eacd96941
baiyu <rc_baiyu@163.com>
2021-04-07 15:25:24 +0800

    day6 practice

---
+++ src/day6/s81/Solution.java
@@ -0,0 +1,41 @@
+package day6.s81;
+
+/**
+ * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+ */
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target)
+                return true;
+            if (nums[start] == nums[mid]) {
+                start++;
+                continue;
+            }
+            if (nums[start] < nums[mid]) {
+                if (target >= nums[start] && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.search(new int[]{3, 1}, 1));
+    }
+}
