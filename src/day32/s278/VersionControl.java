c6b30116693661f376649abd95b2f3edc264c2ed
baiyu <rc_baiyu@163.com>
2021-06-15 15:40:19 +0800

    day32 practice

--- src/day32/s278/VersionControl.java
+++ src/day32/s278/VersionControl.java
@@ -1,7 +1,13 @@
 package day32.s278;
 
 public class VersionControl {
+    int badVersion;
+
+    public VersionControl(int badVersion) {
+        this.badVersion = badVersion;
+    }
+
     boolean isBadVersion(int version) {
-        return true;
+        return badVersion <= version;
     }
 }
