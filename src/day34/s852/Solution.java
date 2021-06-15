6f936ff0f7c1a7a2865bd640bca254c9c7cc058d
baiyu <rc_baiyu@163.com>
2021-06-15 15:52:36 +0800

    day34 practice

---
+++ src/day34/s852/Solution.java
@@ -0,0 +1,30 @@
+package day34.s852;
+
+/**
+ * 符合下列属性的数组 arr 称为 山脉数组 ：
+ * arr.length >= 3
+ * 存在 i（0 < i< arr.length - 1）使得：
+ * arr[0] < arr[1] < ... arr[i-1] < arr[i]
+ * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ * 给你由整数组成的山脉数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i 。
+ */
+public class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.peakIndexInMountainArray(new int[]{0, 1, 0}));
+    }
+}
